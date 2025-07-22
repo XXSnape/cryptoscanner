4074d3a6c82e1d098629bdb8161cb0a8607059bb
Snape <serg.serg2006@mail.ru>
2025-07-22 18:19:11 +0300

    Write integration tests

---
+++ tests/integration/test_wallets.py
@@ -0,0 +1,35 @@
+import httpx
+from httpx import AsyncClient
+
+
+async def test_send_and_get_wallet_information(
+    ac: AsyncClient,
+    real_wallet_address: str,
+) -> None:
+
+    response = await ac.post(
+        f"/wallets/{real_wallet_address}",
+    )
+    assert response.status_code == httpx.codes.CREATED
+    json = response.json()
+    assert "balance_trx" in json
+    assert "bandwidth" in json
+    assert "energy" in json
+    assert "date_and_time" in json
+
+    response = await ac.get(
+        f"/wallets/{real_wallet_address}",
+    )
+    assert response.json() == {
+        "items": [json],
+        "total_count": 1,
+        "page": 1,
+        "pages": 1,
+    }
+    response = await ac.post(
+        f"/wallets/abc",
+    )
+    assert response.status_code == httpx.codes.NOT_FOUND
+    assert response.json() == {
+        "detail": "Address not found or invalid."
+    }
